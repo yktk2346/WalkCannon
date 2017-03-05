00ac392b33ada43c553437b9fb4c68c38e7e1ad7
ヨコタタクヤ <yktk2346@gmail.com>
2017-03-05 18:22:16 +0900

    自己製作(WalkCannon)プロジェクト
    確認をよろしくお願いいたします。

---
+++ Assets/Ball.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    Rigidbody rb;
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+
+        //ボールのVector3に前へ進む力を加え、Rigidbodyを取得し、それに代入
+        Vector3 force;
+        force = this.gameObject.transform.forward * speed;
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(force);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter(Collision other) {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/CannonController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonController : MonoBehaviour {
+
+    //CannonBallを生成するための変数
+    public GameObject cannonBall;
+    //砲弾の発射地点の変数、生成位置を指定
+    public Transform muzzle;
+
+    //Cannonを移動させるためのコンポーネントを入れる
+    private Rigidbody myrb;
+
+    //左ボタン押下の判定
+    private bool isLButtonDown = false;
+    //右ボタン押下の判定
+    private bool isRButtonDown = false;
+    //左の移動できる範囲
+    private float canMoveL = 0f;
+    //右の移動できる範囲
+    private float canMoveR = 150f;
+    //左右に移動するための力
+    private float turnForce = 50f;
+
+
+	// Use this for initialization
+	void Start () {
+        //Rigidbodyを取得
+        myrb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //スペースキーが押された時
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            //砲弾を複製
+            Instantiate(cannonBall, muzzle.position, muzzle.rotation);
+        }
+
+        //Cannonをボタンに応じて移動させる
+        //※移動上限未設定、後から修正の必要あり
+        if ((isLButtonDown) && canMoveL < this.transform.position.x)
+        {
+            //左に移動
+            this.myrb.AddForce(-this.turnForce, 0, 0);
+        }
+        else if ((isRButtonDown) && canMoveR > this.transform.position.x) {
+            //右に移動
+            this.myrb.AddForce(this.turnForce, 0, 0);
+        }
+	}
+}
